665c18ee69b94bc828548d0709f91ccd81812829
Nawaz <141501933+Nawaz-B-04@users.noreply.github.com>
2025-03-08 22:42:39 +0530

    Update Union_of_two_sorted_arrays.cpp

--- Arrays/easy/Union_of_two_sorted_arrays.cpp
+++ Arrays/easy/Union_of_two_sorted_arrays.cpp
@@ -1,1 +1,82 @@
+//Problem : Given two sorted arrays, arr1, and arr2 of size n and m. Find the union of two sorted arrays.
+//Solution 1 - 
+#include <bits/stdc++.h>
+using namespace std;
+
+vector < int > FindUnion(int arr1[], int arr2[], int n, int m) {
+  map < int, int > freq;
+  vector < int > Union;
+  for (int i = 0; i < n; i++)
+    freq[arr1[i]]++;
+  for (int i = 0; i < m; i++)
+    freq[arr2[i]]++;
+  for (auto & it: freq)
+    Union.push_back(it.first);
+  return Union;
+}
+
+int main() {
+  int n = 10, m = 7;
+  int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int arr2[] = {2, 3, 4, 4, 5, 11, 12};
+  vector < int > Union = FindUnion(arr1, arr2, n, m);
+  cout << "Union of arr1 and arr2 is " << endl;
+  for (auto & val: Union)
+    cout << val << " ";
+  return 0;
+}
+
+#Time Compleixty : O((m+n)log(m+n))
+#Space Complexity : O(1)
+
+//Solution 2 -
+#include <bits/stdc++.h>
+using namespace std;
+
+vector < int > FindUnion(int arr1[], int arr2[], int n, int m) {
+  int i = 0, j = 0; 
+  vector < int > Union; 
+  while (i < n && j < m) {
+    if (arr1[i] <= arr2[j])
+    {
+      if (Union.size() == 0 || Union.back() != arr1[i])
+        Union.push_back(arr1[i]);
+      i++;
+    } else 
+    {
+      if (Union.size() == 0 || Union.back() != arr2[j])
+        Union.push_back(arr2[j]);
+      j++;
+    }
+  }
+  while (i < n) 
+  {
+    if (Union.back() != arr1[i])
+      Union.push_back(arr1[i]);
+    i++;
+  }
+  while (j < m) 
+  {
+    if (Union.back() != arr2[j])
+      Union.push_back(arr2[j]);
+    j++;
+  }
+  return Union;
+}
+int main()
+
+{
+  int n = 10, m = 7;
+  int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int arr2[] = {2, 3, 4, 4, 5, 11, 12}; 
+  vector < int > Union = FindUnion(arr1, arr2, n, m);
+  cout << "Union of arr1 and arr2 is  " << endl;
+  for (auto & val: Union)
+    cout << val << " ";
+  return 0;
+}
+
+# Time Complexity: O(m+n)
+# Space Complexity : O(m+n) 
+
 
