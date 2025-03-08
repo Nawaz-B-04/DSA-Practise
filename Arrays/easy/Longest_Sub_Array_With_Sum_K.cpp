604d499abe45a6ef1e2cce33bdd8df23d7791668
Nawaz <141501933+Nawaz-B-04@users.noreply.github.com>
2025-03-08 22:59:19 +0530

    Updated brute and optimal solution for Longest_Sub_Array_With_Sum_K.cpp

--- Arrays/easy/Longest_Sub_Array_With_Sum_K.cpp
+++ Arrays/easy/Longest_Sub_Array_With_Sum_K.cpp
@@ -1,1 +1,77 @@
+//Problem Statement : Longest Subarray with sum K
 
+//1) Brute Force :
+#include <bits/stdc++.h>
+using namespace std;
+
+int getLongestSubarray(vector<int>& a, int k) {
+    int n = a.size(); 
+
+    int len = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) { 
+            int s = 0;
+            for (int K = i; K <= j; K++) {
+                s += a[K];
+            }
+            if (s == k)
+                len = max(len, j - i + 1);
+        }
+    }
+    return len;
+}
+
+int main()
+{
+    vector<int> a = { -1, 1, 1};
+    int k = 1;
+    int len = getLongestSubarray(a, k);
+    cout << "The length of the longest subarray is: " << len << "\n";
+    return 0;
+}
+#Time Complexity: O(N3)
+#Space Complexity: O(1)
+
+//Optimal Solution -
+#include <bits/stdc++.h>
+using namespace std;
+
+int getLongestSubarray(vector<int>& a, int k) {
+    int n = a.size(); 
+
+    map<int, int> preSumMap;
+    int sum = 0;
+    int maxLen = 0;
+    for (int i = 0; i < n; i++) {
+     
+        sum += a[i];
+
+        if (sum == k) {
+            maxLen = max(maxLen, i + 1);
+        }
+
+        int rem = sum - k;
+
+        if (preSumMap.find(rem) != preSumMap.end()) {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+
+        if (preSumMap.find(sum) == preSumMap.end()) {
+            preSumMap[sum] = i;
+        }
+    }
+
+    return maxLen;
+}
+
+int main()
+{
+    vector<int> a = { -1, 1, 1};
+    int k = 1;
+    int len = getLongestSubarray(a, k);
+    cout << "The length of the longest subarray is: " << len << "\n";
+    return 0;
+}
+#Time Complexity :O(N) or O(N*logN)
+#Space Complexity: O(N)
