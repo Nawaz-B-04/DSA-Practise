5bc9050b1282b6a2098bed199273d86332ba1b26
Nawaz <141501933+Nawaz-B-04@users.noreply.github.com>
2025-03-10 15:52:23 +0530

    Update Kadanes_Algorithm.cpp

--- Arrays/Medium/Kadanes_Algorithm.cpp
+++ Arrays/Medium/Kadanes_Algorithm.cpp
@@ -1,1 +1,63 @@
+//Problem : Kadane's Algorithm : Maximum Subarray Sum in an Array
+
+//Approach 1 -
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxSubarraySum(int arr[], int n) {
+    int maxi = INT_MIN; 
+
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = i; j < n; j++) {
+            sum += arr[j];
+
+            maxi = max(maxi, sum); 
+        }
+    }
+
+    return maxi;
+}
+
+int main()
+{
+    int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int maxSum = maxSubarraySum(arr, n);
+    cout << "The maximum subarray sum is: " << maxSum << endl;
+    return 0;
+}
+
+#Time Complexity: O(N2)
+#Space Complexity: O(1)
+
+//Optimal Approach-
+#include <bits/stdc++.h>
+using namespace std;
+
+long long maxSubarraySum(int arr[], int n) {
+    long long maxi = LONG_MIN; // maximum sum
+    long long sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+        if (sum > maxi) {
+            maxi = sum;
+        }
+        if (sum < 0) {
+            sum = 0;
+        }
+    }
+    return maxi;
+}
+int main()
+{
+    int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    long long maxSum = maxSubarraySum(arr, n);
+    cout << "The maximum subarray sum is: " << maxSum << endl;
+    return 0;
+}
+#Time Complexity: O(N)
+#Space Complexity: O(1)
 
