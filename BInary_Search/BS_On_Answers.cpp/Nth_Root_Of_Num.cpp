fec2d07901cb62c789b416989174942280b0ebe4
Nawaz <141501933+Nawaz-B-04@users.noreply.github.com>
2025-03-17 23:46:59 +0530

    Update Nth_Root_Of_Num.cpp

--- BInary_Search/BS_On_Answers.cpp/Nth_Root_Of_Num.cpp
+++ BInary_Search/BS_On_Answers.cpp/Nth_Root_Of_Num.cpp
@@ -1,1 +1,41 @@
 //Problem Statement: Given two numbers N and M, find the Nth root of M. The nth root of a number M is defined as a number X when raised to the power N equals M. If the 'nth root is not an integer, return -1.
+#include <bits/stdc++.h>
+using namespace std;
+
+//return 1, if == m:
+//return 0, if < m:
+//return 2, if > m:
+int func(int mid, int n, int m) {
+    long long ans = 1;
+    for (int i = 1; i <= n; i++) {
+        ans = ans * mid;
+        if (ans > m) return 2;
+    }
+    if (ans == m) return 1;
+    return 0;
+}
+
+int NthRoot(int n, int m) {
+    //Use Binary search on the answer space:
+    int low = 1, high = m;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int midN = func(mid, n, m);
+        if (midN == 1) {
+            return mid;
+        }
+        else if (midN == 0) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;
+}
+
+int main()
+{
+    int n = 3, m = 27;
+    int ans = NthRoot(n, m);
+    cout << "The answer is: " << ans << "\n";
+    return 0;
+}
+#Time Complexity: O(logN)
+#Space Complexity: O(1)
